478f613024196e9d05ec105fb569b422f3283bed
Anees Ahmad <aneesahmadweb@gmail.com>
2023-04-12 22:14:23 +0500

    first commit

---
+++ Rewrite/Magento/Sales/Order/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Ecommerce Module All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Winstonmarriot\Picopc\Rewrite\Magento\Sales\Order;
+
+class Index extends \Magento\Sales\Order\Index
+{
+
+}
+
